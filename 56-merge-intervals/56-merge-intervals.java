e60f7d288df5aff2cafc321ac162126919a8cac5
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-09-01 12:59:22 +0530

    Time: 7 ms (99.31%), Space: 47.4 MB (89.97%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,21 +1,20 @@
 class Solution {
+
     public int[][] merge(int[][] intervals) {
-        if(intervals.length<=1) return intervals;
-        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
-        List<int[]> ans=new ArrayList<>();
+        if (intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> ans = new ArrayList<>();
         ans.add(intervals[0]);
-        int j=0;
-        for(int i=1;i<intervals.length;i++)
-        {
-            int[] current=ans.get(j);
-            if(current[1]>=intervals[i][0])
-            {
-                current[1]=Math.max(intervals[i][1],current[1]);
-            }else{
+        int j = 0;
+        for (int i = 1; i < intervals.length; i++) {
+            int[] current = ans.get(j);
+            if (current[1] >= intervals[i][0]) {
+                current[1] = Math.max(intervals[i][1], current[1]);
+            } else {
                 ans.add(intervals[i]);
                 j++;
             }
         }
-    return ans.toArray(new int[ans.size()][]);
+        return ans.toArray(new int[ans.size()][]);
     }
 }
