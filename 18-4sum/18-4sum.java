9742bb95421fc26f25432c08b01918070ad0f6c5
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-09-01 10:20:30 +0530

    Time: 49 ms (26.90%), Space: 44.5 MB (63.95%) - LeetHub

---
+++ 18-4sum/18-4sum.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        ArrayList<List<Integer>> res = new ArrayList<>();
+        if (nums == null || nums.length == 0) {
+            return res;
+        }
+        int n = nums.length;
+        Arrays.sort(nums);
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+               
+                int front = j + 1;
+                int back = n - 1;
+                while (front < back) {
+                    long two_sum = nums[front];
+                    two_sum+=nums[back];
+                      two_sum+=nums[i];
+                      two_sum+=nums[j];
+                    if (two_sum < target) front++; else if (two_sum > target) back--; else {
+                        List<Integer> quad = new ArrayList<>();
+                        quad.add(nums[i]);
+                        quad.add(nums[j]);
+                        quad.add(nums[front]);
+                        quad.add(nums[back]);
+                        res.add(quad);
+
+                        while (front < back && nums[front] == quad.get(2)) ++front;
+                        while (front < back && nums[back] == quad.get(3)) --back;
+                    }
+                }
+                while (j + 1 < n && nums[j + 1] == nums[j]) ++j;
+            }
+            while (i + 1 < n && nums[i + 1] == nums[i]) ++i;
+        }
+        return res;
+    }
+}
