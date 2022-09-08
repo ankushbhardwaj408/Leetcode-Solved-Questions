bcbea4fb0435f0618ee328d7c834a38e9e94597e
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-09-08 12:48:23 +0530

    Time: 13 ms (77.64%), Space: 53.4 MB (84.81%) - LeetHub

---
+++ 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numWays(String s) {
+        long ones=0;
+        long n=s.length();
+        long MOD=1_000_000_007;
+        for(char c:s.toCharArray())
+        {
+            ones+=c - '0';
+        }
+   
+        if(ones==0) return (int)(((n-1)*(n-2))/2%MOD);
+        if(ones%3!=0) return 0;
+             long ot=ones/3;
+        ones=0;
+        long ways1=0;
+        long ways2=0;
+        for(char c:s.toCharArray())
+        {
+             ones+=c-'0';
+          if(ones==ot) ways1++;
+            if(ones==2*ot)ways2++;
+        }
+        return (int)((ways1*ways2)%MOD);
+    }
+}
