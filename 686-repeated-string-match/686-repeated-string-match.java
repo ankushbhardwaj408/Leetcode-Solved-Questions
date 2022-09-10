94710c95544a93285767e3b7180c9f73af62123f
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-09-10 13:34:53 +0530

    Time: 346 ms (50.00%), Space: 113.1 MB (37.86%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,13 +1,13 @@
 class Solution {
+
     public int repeatedStringMatch(String a, String b) {
-       if(b=="") return 0;
-        String str=a;
-        int repeat=b.length()/a.length();
-        int count=1;
-        for(int i=0;i<repeat+2;i++)
-        {
-            if(a.contains(b)) return count;
-            a=a+str;
+        if (b == "") return 0;
+        String str = a;
+        int repeat = b.length() / a.length();
+        int count = 1;
+        for (int i = 0; i < repeat + 2; i++) {
+            if (a.contains(b)) return count;
+            a = a + str;
             count++;
         }
         return -1;
