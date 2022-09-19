c241ee3710b04dce00629f08642714c9f16a28fc
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-09-19 16:12:50 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (91.09%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -14,13 +14,14 @@
  * }
  */
 class Solution {
+
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null) return true;
-        if(p==null) return false;
-        if(q==null) return false;
-        if(p.val!=q.val) return false;
-        boolean x=isSameTree(p.left,q.left);
-         boolean y=isSameTree(p.right,q.right);
-        return (x&y);
+        if (p == null && q == null) return true;
+        if (p == null) return false;
+        if (q == null) return false;
+        if (p.val != q.val) return false;
+        boolean x = isSameTree(p.left, q.left);
+        boolean y = isSameTree(p.right, q.right);
+        return (x & y);
     }
 }
