17aac0e1d6a5b3c5095370d732a262b4ebae0e9c
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-09-19 15:41:40 +0530

    Time: 141 ms (81.17%), Space: 68.6 MB (37.57%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -1,28 +1,26 @@
 class Solution {
+
     public int countPrimes(int n) {
-        if(n==0) return 0;
-        if(n==1) return 0;
-        if(n==2) return 0;
-        boolean[] ans=new boolean[n+1];
-      int x=  prime(n,ans);
-        
+        if (n == 0) return 0;
+        if (n == 1) return 0;
+
+        boolean[] ans = new boolean[n + 1];
+        int x = prime(n, ans);
+
         return x;
     }
-    public int prime(int n,boolean[] ans)
-    {
-        for(int i=2;i<=Math.sqrt(n);i++)
-        {
-            if(!ans[i]){
-                for(int j=i*2;j<=n;j+=i)
-                {
-                    ans[j]=true;
+
+    public int prime(int n, boolean[] ans) {
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (!ans[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    ans[j] = true;
                 }
             }
         }
-        int c=0;
-        for(int k=2;k<n;k++)
-        {
-            if(!ans[k]) c++;
+        int c = 0;
+        for (int k = 2; k < n; k++) {
+            if (!ans[k]) c++;
         }
         return c;
     }
