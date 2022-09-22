b6c1d4db321a4a263b28ba8eba09de965bbad5ba
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-09-22 13:49:25 +0530

    Time: 0 ms (100.00%), Space: 49.7 MB (52.73%) - LeetHub

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -14,26 +14,27 @@
  * }
  */
 class Solution {
+
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root==null) return root;
-        if(key<root.val){
-            root.left=deleteNode(root.left,key);
-        }else if(key>root.val){
-            root.right=deleteNode(root.right,key);
-        }else{
-            if(root.left==null) return root.right;
-            if(root.right==null) return root.left;
-            root.val=min(root.right);
-            root.right=deleteNode(root.right,root.val);
+        if (root == null) return root;
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else {
+            if (root.left == null) return root.right;
+            if (root.right == null) return root.left;
+            root.val = min(root.right);
+            root.right = deleteNode(root.right, root.val);
         }
         return root;
     }
-    
-    public int min(TreeNode root){
-        int mini=0;
-        while(root!=null){
-            mini=root.val;
-            root=root.left;
+
+    public int min(TreeNode root) {
+        int mini = 0;
+        while (root != null) {
+            mini = root.val;
+            root = root.left;
         }
         return mini;
     }
