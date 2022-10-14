2c180ff94b26e9de5ece467a4920254aa2272069
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-10-14 16:15:45 +0530

    Time: 10 ms (38.49%), Space: 61.1 MB (76.15%) - LeetHub

--- 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.java
+++ 1616-split-two-strings-to-make-palindrome/1616-split-two-strings-to-make-palindrome.java
@@ -1,25 +1,22 @@
 class Solution {
+
     public boolean checkPalindromeFormation(String a, String b) {
-        return (split(a,b) || split(b,a));
+        return (split(a, b) || split(b, a));
     }
-    public boolean split(String a,String b)
-    {
-        int i=0;
-        int j=a.length()-1;
-        while(  i<=j && a.charAt(i)==b.charAt(j))
-        {
+
+    public boolean split(String a, String b) {
+        int i = 0;
+        int j = a.length() - 1;
+        while (i <= j && a.charAt(i) == b.charAt(j)) {
             i++;
             j--;
-            
         }
-    return (palin(a,i,j) || palin(b,i,j));
-        
+        return (palin(a, i, j) || palin(b, i, j));
     }
-    public boolean palin(String s,int a,int b)
-    {
-        while(a<=b)
-        {
-            if(s.charAt(a)!=s.charAt(b)) return false;
+
+    public boolean palin(String s, int a, int b) {
+        while (a <= b) {
+            if (s.charAt(a) != s.charAt(b)) return false;
             a++;
             b--;
         }
