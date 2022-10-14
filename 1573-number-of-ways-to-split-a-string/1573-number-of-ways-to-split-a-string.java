c7b43e60fe513fdcb42b0d5f299123a279e33b4a
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2022-10-14 16:40:55 +0530

    Time: 19 ms (47.44%), Space: 53.8 MB (80.93%) - LeetHub

--- 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.java
+++ 1573-number-of-ways-to-split-a-string/1573-number-of-ways-to-split-a-string.java
@@ -1,24 +1,25 @@
 class Solution {
-
     public int numWays(String s) {
-        long ones = 0;
-        long n = s.length();
-        long MOD = 1_000_000_007;
+        long ones=0;
+        long n=s.length();
+        long Mod=1_000_000_007;
         for (char c : s.toCharArray()) {
             ones += c - '0';
         }
-
-        if (ones == 0) return (int) (((n - 1) * (n - 2)) / 2 % MOD);
-        if (ones % 3 != 0) return 0;
-        long ot = ones / 3;
-        ones = 0;
-        long ways1 = 0;
-        long ways2 = 0;
-        for (char c : s.toCharArray()) {
-            ones += c - '0';
-            if (ones == ot) ways1++;
-            if (ones == 2 * ot) ways2++;
+        if(ones==0) return (int)(((n-1)*(n-2))/2%Mod);
+        if(ones%3!=0) return 0;
+        
+        long req=ones/3;
+        ones=0;
+        long way1=0;
+        long way2=0;
+         for(int j=0;j<s.length();j++)
+        {
+            if(s.charAt(j)=='1') ones++;
+             if(ones==req)way1++;
+              if(ones==2*req)way2++;
+             
         }
-        return (int) ((ways1 * ways2) % MOD);
+        return (int) ((way1*way2)%Mod);
     }
 }
