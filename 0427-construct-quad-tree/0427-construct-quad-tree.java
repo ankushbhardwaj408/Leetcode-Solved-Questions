28659494cbf5aee29dfb8b453d72804160f237bf
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2023-02-27 22:19:33 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (56.51%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.java
+++ 0427-construct-quad-tree/0427-construct-quad-tree.java
@@ -41,14 +41,13 @@ class Node {
 class Solution {
 
     public Node construct(int[][] grid) {
-        int x=grid.length;
-        
-        Node ans=fun(grid,0,0,x,x);
+        int x = grid.length;
+
+        Node ans = fun(grid, 0, 0, x, x);
         return ans;
     }
 
     public Node fun(int[][] grid, int rs, int cs, int re, int ce) {
-       
         if (gridfun(grid, rs, cs, re, ce)) {
             boolean x;
             if (grid[rs][cs] == 1) {
@@ -58,26 +57,25 @@ class Solution {
             }
             Node node = new Node(x, true);
             return node;
-           
         }
-        if((re-rs)==2 && (ce-cs)==2){
-            boolean a=(grid[rs][cs]==1);
-             boolean b=(grid[rs][ce-1]==1);
-             boolean c=(grid[re-1][cs]==1);
-             boolean d=(grid[re-1][ce-1]==1);
-            Node node1=new Node(a,true);
-             Node node2=new Node(b,true);
-             Node node3=new Node(c,true);
-             Node node4=new Node(d,true);
-           Node root=new Node(true,false,node1,node2,node3,node4);
+        if ((re - rs) == 2 && (ce - cs) == 2) {
+            boolean a = (grid[rs][cs] == 1);
+            boolean b = (grid[rs][ce - 1] == 1);
+            boolean c = (grid[re - 1][cs] == 1);
+            boolean d = (grid[re - 1][ce - 1] == 1);
+            Node node1 = new Node(a, true);
+            Node node2 = new Node(b, true);
+            Node node3 = new Node(c, true);
+            Node node4 = new Node(d, true);
+            Node root = new Node(true, false, node1, node2, node3, node4);
             return root;
         }
-        
-        Node topleft=fun(grid,rs,cs,rs+(re-rs)/2,cs+(ce-cs)/2);
-         Node topright=fun(grid,rs,cs+(ce-cs)/2,rs+(re-rs)/2,ce);
-         Node bottomleft=fun(grid,rs+(re-rs)/2,cs,re,cs+(ce-cs)/2);
-         Node bottomright=fun(grid,rs+(re-rs)/2,cs+(ce-cs)/2,re,ce);
-        Node ans=new Node(true,false,topleft,topright,bottomleft,bottomright);
+
+        Node topleft = fun(grid, rs, cs, rs + (re - rs) / 2, cs + (ce - cs) / 2);
+        Node topright = fun(grid, rs, cs + (ce - cs) / 2, rs + (re - rs) / 2, ce);
+        Node bottomleft = fun(grid, rs + (re - rs) / 2, cs, re, cs + (ce - cs) / 2);
+        Node bottomright = fun(grid, rs + (re - rs) / 2, cs + (ce - cs) / 2, re, ce);
+        Node ans = new Node(true, false, topleft, topright, bottomleft, bottomright);
         return ans;
     }
 
