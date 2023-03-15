36fb5d840abce7c3d09d4f1f3f9f4e2197715baf
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2023-03-15 21:16:03 +0530

    Time: 1 ms (100.00%), Space: 42.2 MB (26.75%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -19,99 +19,63 @@ class Solution {
     }
 
     public int orangesRotting(int[][] nums) {
-        
         int ans = 0;
-        if(fun2(nums))return 0;
+        if (fun2(nums)) return 0;
         Queue<TreeNode> q = new LinkedList<>();
         for (int i = 0; i < nums.length; i++) {
             for (int j = 0; j < nums[0].length; j++) {
-               
                 if (nums[i][j] == 2) {
                     q.add(new TreeNode(0, i, j));
                 }
             }
         }
-        if(q.isEmpty())return -1;
+        if (q.isEmpty()) return -1;
         while (!q.isEmpty()) {
             TreeNode node = q.poll();
-            int val=node.val;
-            ans=val;
-            int x=node.x;
-            int y=node.y;
+            int val = node.val;
+            ans = val;
+            int x = node.x;
+            int y = node.y;
             if (x + 1 < nums.length) {
-               if(nums[x+1][y]==1){
-                   q.add(new TreeNode(val+1,x+1,y));
-                   nums[x+1][y]=2;
-               }
+                if (nums[x + 1][y] == 1) {
+                    q.add(new TreeNode(val + 1, x + 1, y));
+                    nums[x + 1][y] = 2;
                 }
-            
-             if (x -1 >=0) {
-               if(nums[x-1][y]==1){
-                   q.add(new TreeNode(val+1,x-1,y));
-                   nums[x-1][y]=2;
-               }
-                }
-            
-             if (y + 1 < nums[0].length) {
-               if(nums[x][y+1]==1){
-                   q.add(new TreeNode(val+1,x,y+1));
-                   nums[x][y+1]=2;
-                   
-               }
-                }
-            
-             if (y-1 >=0) {
-               if(nums[x][y-1]==1){
-                   q.add(new TreeNode(val+1,x,y-1));
-                   nums[x][y-1]=2;
-               }
-                }
-            
-            
-            
             }
-        for(int a=0;a<nums.length;a++)
-        {
-            for(int b=0;b<nums[0].length;b++)
-            {
-                if(nums[a][b]==1)return -1;
-            }
-        }
-        return ans;
-        }
-    
 
-    public boolean fun(int[][] nums, int i, int j) {
-        boolean ans = true;
-        if (i + 1 < nums.length) {
-            if (nums[i + 1][j] != 0) {
-                ans = false;
+            if (x - 1 >= 0) {
+                if (nums[x - 1][y] == 1) {
+                    q.add(new TreeNode(val + 1, x - 1, y));
+                    nums[x - 1][y] = 2;
+                }
+            }
+
+            if (y + 1 < nums[0].length) {
+                if (nums[x][y + 1] == 1) {
+                    q.add(new TreeNode(val + 1, x, y + 1));
+                    nums[x][y + 1] = 2;
+                }
+            }
+
+            if (y - 1 >= 0) {
+                if (nums[x][y - 1] == 1) {
+                    q.add(new TreeNode(val + 1, x, y - 1));
+                    nums[x][y - 1] = 2;
+                }
             }
         }
-        if (i - 1 >= 0) {
-            if (nums[i - 1][j] != 0) {
-                ans = false;
-            }
-        }
-        if (j + 1 < nums[0].length) {
-            if (nums[i][j + 1] != 0) {
-                ans = false;
-            }
-        }
-        if (j-1 >= 0) {
-            if (nums[i][j-1] != 0) {
-                ans = false;
+        for (int a = 0; a < nums.length; a++) {
+            for (int b = 0; b < nums[0].length; b++) {
+                if (nums[a][b] == 1) return -1;
             }
         }
         return ans;
     }
-    public boolean fun2(int[][] nums)
-    {
-        for(int i=0;i<nums.length;i++)
-        {
-            for(int j=0;j<nums[0].length;j++)
-            {
-                if(nums[i][j]!=0)return false;
+
+    public boolean fun2(int[][] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums[0].length; j++) {
+                if (nums[i][j] != 0) return false;
             }
         }
         return true;
