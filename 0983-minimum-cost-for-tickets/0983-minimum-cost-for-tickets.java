1c88dc452b9f697eec64df027400c1393fc2b48b
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2023-03-28 20:44:03 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (42.84%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.java
@@ -1,36 +1,26 @@
 class Solution {
-     Integer[] dp;
-        public int mincostTickets(int[] days, int[] costs) {
-            dp = new Integer[days.length];
-            return helper(days, costs, 0);
-        }
-
-        private int helper(int[] days, int[] costs, int day) {
-            if(day >= days.length)
-                return 0;
-
-            if(dp[day] != null)
-                return dp[day];
-
-            int ondDayPass = helper(days, costs, day + 1) + costs[0];
-
-            int i;
-            for(i = day; i < days.length; i++){
-                if(days[i] >= days[day] + 7)
-                    break;
-            }
-                
-
-            int sevenDayPass = helper(days, costs, i) + costs[1];
-
-            for(i = day; i < days.length; i++){
-                 if(days[i] >= days[day] + 30)
-                    break;
-            }
-               
-
-            int thirtyDayPass = helper(days, costs, i) + costs[2];
-
-            return dp[day] = Math.min(ondDayPass, Math.min(sevenDayPass, thirtyDayPass));
-        }
+    Integer[] arr;
+    public int mincostTickets(int[] days, int[] costs) {
+        arr=new Integer[days.length];
+        return fun(days,costs,0);
     }
+    public int fun(int[] days, int[] costs,int d){
+        if(d>=days.length)return 0;
+        if(arr[d]!=null){
+            return arr[d];
+        }
+        int one=fun(days,costs,d+1)+costs[0];
+        int i;
+        for(i=d;i<days.length;i++)
+        {
+            if(days[i]>=days[d]+7)break;
+        }
+        int two=fun(days,costs,i)+costs[1];
+         for(i=d;i<days.length;i++)
+        {
+            if(days[i]>=days[d]+30)break;
+        }
+        int three=fun(days,costs,i)+costs[2];
+        return arr[d]= Math.min(one,Math.min(two,three));
+    }
+}
