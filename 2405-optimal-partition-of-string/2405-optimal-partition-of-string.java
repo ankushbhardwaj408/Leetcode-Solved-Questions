23ab7a09de596381cb7e22e0b480cb878cd8b374
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2023-05-07 07:41:19 +0530

    Time: 39 ms (35.61%), Space: 43.3 MB (50.89%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -3,7 +3,7 @@ class Solution {
     public int partitionString(String s) {
       int count=(s.isEmpty())? 0:1;
         
-        s=s.toLowerCase();
+      
         HashSet<Character> letter =new HashSet<Character>();
         for(int i=0;i<s.length();i++){
             if(letter.contains(s.charAt(i))){
