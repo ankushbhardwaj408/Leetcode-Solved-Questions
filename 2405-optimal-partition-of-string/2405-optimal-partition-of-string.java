fb7bb8861cd9cffab35946cee4fa2efd2371b350
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2023-05-07 07:42:10 +0530

    Time: 41 ms (16.80%), Space: 43.4 MB (41.37%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,19 +1,16 @@
 class Solution {
-    static int ans=Integer.MAX_VALUE;
+
     public int partitionString(String s) {
-        if(s.length()==0)return 0;
-      int count=1;
-        
-      
-        HashSet<Character> letter =new HashSet<Character>();
-        for(int i=0;i<s.length();i++){
-            if(letter.contains(s.charAt(i))){
+        if (s.length() == 0) return 0;
+        int count = 1;
+        HashSet<Character> letter = new HashSet<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            if (letter.contains(s.charAt(i))) {
                 letter.clear();
-                count ++;
+                count++;
             }
             letter.add(s.charAt(i));
         }
         return count;
     }
-  
-    }
+}
