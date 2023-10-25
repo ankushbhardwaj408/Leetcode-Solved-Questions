3b249929d7a29369e3f1baa9a8e227f7ec764cca
Ankush Bhardwaj <93874580+ankushbhardwaj408@users.noreply.github.com>
2023-10-25 13:04:53 +0530

    Time: 142 ms (5.86%), Space: 57.5 MB (30.97%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,27 +1,47 @@
 class Solution {
-    public int largestRectangleArea(int[] heights) {
-      int area = 0, n = heights.length;
-      int start;
-      Stack<Pair<Integer,Integer>> stack = new Stack<>();
-      for(int i=0;i<heights.length;i++){
-         int curHt =heights[i];
-         start = i;
-        while(!stack.isEmpty() && stack.peek().getValue() > curHt){
-          Pair<Integer,Integer> pair = stack.pop();
-          int index = pair.getKey();
-          int h = pair.getValue();
-          area = Math.max(area, h * (i - index));
-          start = index;
+    public int largestRectangleArea(int[] nums) {
+        int[] next=new int[nums.length];
+        int[] prev=new int[nums.length];
+        Arrays.fill(next,nums.length);
+        Arrays.fill(prev,-1);
+        Stack<Integer> st1=new Stack<>();
+        st1.push(0);
+        for(int i=1;i<nums.length;i++)
+        {
+            if(!st1.isEmpty() && nums[st1.peek()]<=nums[i]){
+                st1.push(i);
+            }
+                while(!st1.isEmpty() && nums[st1.peek()]>nums[i])
+                {
+                    next[st1.peek()]=i;
+                    st1.pop();
+                }
+                st1.push(i);
+            
         }
-        stack.push(new Pair(start,curHt));
-      }
-      
-       while(!stack.isEmpty()){
-          Pair<Integer,Integer> pair = stack.pop();
-          int index = pair.getKey();
-          int h = pair.getValue();
-          area = Math.max(area, h * (n - index));
-       }
-        return area;
+        Stack<Integer> st2=new Stack<>();
+        st2.push(nums.length-1);
+        for(int i=nums.length-2;i>=0;i--)
+        {
+            if(!st2.isEmpty() && nums[st2.peek()]<=nums[i]){
+                st2.push(i);
+            }
+                while(!st2.isEmpty() && nums[st2.peek()]>nums[i])
+                {
+                    prev[st2.peek()]=i;
+                    st2.pop();
+                }
+                st2.push(i);
+            
+        }
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++)
+        {
+            int ans=(next[i]-prev[i]-1)*nums[i];
+            max=Math.max(ans,max);
+            
+        }
+       
+     return max;
     }
 }
